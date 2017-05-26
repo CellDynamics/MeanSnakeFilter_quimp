b0de0af359f7a7c882d499e9d5e7a2a026d5ffe5
Piotr Baniukiewicz <P.Baniukiewicz@warwick.ac.uk>
2017-05-26 12:27:25 +0100

    Updated tests to make possible to run them from jar

--- src/test/java/quimp/plugin/MeanFilter_Param_Test.java
+++ src/test/java/quimp/plugin/MeanFilter_Param_Test.java
@@ -1,9 +1,16 @@
 package quimp.plugin;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.After;
@@ -33,122 +40,168 @@ import uk.ac.warwick.wsbc.quimp.plugin.utils.RoiSaver;
  */
 @RunWith(Parameterized.class)
 public class MeanFilter_Param_Test {
-    private List<Point2d> testcase;
-    private Integer window;
-    private Path testfileName;
-    static final Logger LOGGER = LoggerFactory.getLogger(MeanFilter_Param_Test.class.getName());
+  private List<Point2d> testcase;
+  private Integer window;
+  private Path testfileName;
+  static final Logger LOGGER = LoggerFactory.getLogger(MeanFilter_Param_Test.class.getName());
 
-    /**
-     * Parameterized constructor.
-     * 
-     * Each parameter should be placed as an argument here Every time runner triggers, it will pass
-     * the arguments from parameters we defined to this method
-     * 
-     * @param testFileName test file name
-     * @param window averaging window size
-     * @see uk.ac.warwick.wsbc.quimp.plugin.utils.DataLoader
-     */
-    public MeanFilter_Param_Test(String testFileName, Integer window) {
-        this.testfileName = Paths.get(testFileName);
-        this.window = window;
+  /**
+   * Parameterized constructor.
+   * 
+   * Each parameter should be placed as an argument here Every time runner triggers, it will pass
+   * the arguments from parameters we defined to this method
+   * 
+   * @param testFileName test file name
+   * @param window averaging window size
+   * @see uk.ac.warwick.wsbc.quimp.plugin.utils.DataLoader
+   */
+  public MeanFilter_Param_Test(String testFileName, Integer window) {
+    this.testfileName = Paths.get(testFileName);
+    this.window = window;
+  }
+
+  /**
+   * Called after construction but before tests
+   * 
+   * @throws Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+    String tf = testfileName.getFileName().toString();
+    testcase = new DataLoader(loadResource(getClass().getClassLoader(), tf).toString()).getData();
+  }
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  /**
+   * Set of parameters for tests.
+   * 
+   * see: QuimP-toolbox/Prototyping/59-Shape_filtering/main.m for creating *.dat files
+   * 
+   * @return List of strings with paths to testfiles and smooth parameter
+   * 
+   */
+  @Parameterized.Parameters
+  public static Collection<Object[]> testFiles() {
+    //!>
+        return Arrays.asList(new Object[][] { 
+                { "testData_75.dat", 1 },
+                { "testData_75.dat", 3 },
+                { "testData_75.dat", 5 },
+                { "testData_75.dat", 9 },
+                { "testData_75.dat", 15 },
+
+                { "testData_125.dat", 1 },
+                { "testData_125.dat", 3 },
+                { "testData_125.dat", 5 },
+                { "testData_125.dat", 9 },
+                { "testData_125.dat", 15 },
+
+                { "testData_137.dat", 1 },
+                { "testData_137.dat", 3 },
+                { "testData_137.dat", 5 },
+                { "testData_137.dat", 9 },
+                { "testData_137.dat", 15 },
+
+                { "testData_1.dat", 1 },
+                { "testData_1.dat", 3 },
+                { "testData_1.dat", 5 },
+                { "testData_1.dat", 9 },
+                { "testData_1.dat", 15 }, });
+        //!<
+  }
+
+  /**
+   * Load resource file from either jar or filesystem.
+   * 
+   * <p>If class loader is an object run from jar, this method will make binary copy of resource in
+   * temporary folder and return path to it.
+   * 
+   * <p>This code is taken from
+   * https://stackoverflow.com/questions/941754/how-to-get-a-path-to-a-resource-in-a-java-jar-file
+   * 
+   * @param c class loader
+   * @param resource resource name and relative path
+   * @return path to resource file
+   */
+  public static Path loadResource(ClassLoader c, String resource) {
+    File file = null;
+    URL res = c.getResource(resource);
+    if (res.toString().startsWith("jar:")) {
+      try {
+        InputStream input = c.getResourceAsStream(resource);
+        file = File.createTempFile(new Date().getTime() + "", "");
+        OutputStream out = new FileOutputStream(file);
+        int read;
+        byte[] bytes = new byte[1024];
+
+        while ((read = input.read(bytes)) != -1) {
+          out.write(bytes, 0, read);
+        }
+        out.flush();
+        out.close();
+        input.close();
+        file.deleteOnExit();
+        return file.toPath();
+      } catch (IOException ex) {
+        throw new RuntimeException(ex);
+      }
+    } else {
+      // this will probably work in your IDE, but not from a JAR
+      return Paths.get(res.getFile());
     }
+  }
 
-    /**
-     * Called after construction but before tests
-     * 
-     * @throws Exception
-     */
-    @Before
-    public void setUp() throws Exception {
-        testcase = new DataLoader(testfileName.toString()).getData();
-    }
+  /**
+   * Test of getInterpolationMean method.
+   * 
+   * Pre: original images saved as test_roiSaver_
+   * 
+   * Post:Save image test_getInterpolationMean_* in /tmp/
+   * 
+   * See: QuimP-toolbox/algorithms/src/test/resources/ Interpolate_Test_Analyzer.m for plotting
+   * results
+   * 
+   * See: @see QuimP-toolbox/Prototyping/59-Shape_filtering/main.m for creating *.dat files
+   * 
+   * @throws QuimpPluginException
+   * 
+   */
+  @SuppressWarnings("serial")
+  @Test
+  public void test_getInterpolationMean() throws QuimpPluginException {
+    List<Point2d> out;
+    MeanSnakeFilter_ i = new MeanSnakeFilter_();
+    i.attachData(testcase);
+    i.setPluginConfig(new ParamList() {
+      {
+        put("window", String.valueOf(window));
+      }
+    });
+    out = i.runPlugin();
+    RoiSaver.saveROI("/tmp/test_getInterpolationMean_" + testfileName.getFileName() + "_"
+            + window.toString() + ".tif", out);
+    LOGGER.debug("setUp: " + testcase.toString());
+    if (out.size() < 100)
+      LOGGER.debug("testInterpolate: " + out.toString());
+  }
 
-    /**
-     * @throws java.lang.Exception
-     */
-    @After
-    public void tearDown() throws Exception {
-    }
-
-    /**
-     * Set of parameters for tests.
-     * 
-     * see: QuimP-toolbox/Prototyping/59-Shape_filtering/main.m for creating *.dat files
-     * 
-     * @return List of strings with paths to testfiles and smooth parameter
-     * 
-     */
-    @Parameterized.Parameters
-    public static Collection<Object[]> testFiles() {
-        return Arrays.asList(new Object[][] { { "src/test/resources/testData_75.dat", 1 },
-                { "src/test/resources/testData_75.dat", 3 },
-                { "src/test/resources/testData_75.dat", 5 },
-                { "src/test/resources/testData_75.dat", 9 },
-                { "src/test/resources/testData_75.dat", 15 },
-
-                { "src/test/resources/testData_125.dat", 1 },
-                { "src/test/resources/testData_125.dat", 3 },
-                { "src/test/resources/testData_125.dat", 5 },
-                { "src/test/resources/testData_125.dat", 9 },
-                { "src/test/resources/testData_125.dat", 15 },
-
-                { "src/test/resources/testData_137.dat", 1 },
-                { "src/test/resources/testData_137.dat", 3 },
-                { "src/test/resources/testData_137.dat", 5 },
-                { "src/test/resources/testData_137.dat", 9 },
-                { "src/test/resources/testData_137.dat", 15 },
-
-                { "src/test/resources/testData_1.dat", 1 },
-                { "src/test/resources/testData_1.dat", 3 },
-                { "src/test/resources/testData_1.dat", 5 },
-                { "src/test/resources/testData_1.dat", 9 },
-                { "src/test/resources/testData_1.dat", 15 }, });
-    }
-
-    /**
-     * Test of getInterpolationMean method.
-     * 
-     * Pre: original images saved as test_roiSaver_
-     * 
-     * Post:Save image test_getInterpolationMean_* in /tmp/
-     * 
-     * See: QuimP-toolbox/algorithms/src/test/resources/ Interpolate_Test_Analyzer.m for plotting
-     * results
-     * 
-     * See: @see QuimP-toolbox/Prototyping/59-Shape_filtering/main.m for creating *.dat files
-     * 
-     * @throws QuimpPluginException
-     * 
-     */
-    @SuppressWarnings("serial")
-    @Test
-    public void test_getInterpolationMean() throws QuimpPluginException {
-        List<Point2d> out;
-        MeanSnakeFilter_ i = new MeanSnakeFilter_();
-        i.attachData(testcase);
-        i.setPluginConfig(new ParamList() {
-            {
-                put("window", String.valueOf(window));
-            }
-        });
-        out = i.runPlugin();
-        RoiSaver.saveROI("/tmp/test_getInterpolationMean_" + testfileName.getFileName() + "_"
-                + window.toString() + ".tif", out);
-        LOGGER.debug("setUp: " + testcase.toString());
-        if (out.size() < 100)
-            LOGGER.debug("testInterpolate: " + out.toString());
-    }
-
-    /**
-     * Simple test of RoiSaver class, create reference images without processing.
-     * 
-     * Post: Save image /tmp/testroiSaver_*.tif
-     */
-    @Test
-    @Ignore
-    public void test_roiSaver() {
-        RoiSaver.saveROI("/tmp/test_roiSaver_" + testfileName.getFileName() + "_"
-                + window.toString() + ".tif", testcase);
-    }
+  /**
+   * Simple test of RoiSaver class, create reference images without processing.
+   * 
+   * Post: Save image /tmp/testroiSaver_*.tif
+   */
+  @Test
+  @Ignore
+  public void test_roiSaver() {
+    RoiSaver.saveROI(
+            "/tmp/test_roiSaver_" + testfileName.getFileName() + "_" + window.toString() + ".tif",
+            testcase);
+  }
 
 }
